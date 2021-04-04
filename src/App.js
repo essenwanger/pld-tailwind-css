ac58f1681c5bf50f415d0787ec01a042b923798c
Vic <vic@Vics-MacBook-Pro-2.local>
2021-04-03 19:11:20 -0500

    chore(config): config tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="bg-blue-800">
+      <header>
+        <img src={logo} className="h-20 w-20" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
